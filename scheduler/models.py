aeab250d60e27f94fe6d23fff5dfcba23da1bb2e
Porsil <helenbrown13@hotmail.com>
2023-01-12 20:29:12 +0000

    models added to models.py

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+AVAILABLE_STATUS = (("Active", "Active"), ("Inactive", "Inactive"))
+WORKLOAD_STATUS = (("To Do", "To Do"), ("Completed", "Completed"))
+
+
+class Analyst(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    status = models.TextField(choices=AVAILABLE_STATUS, default="Active")
+
+    def __str__(self):
+        return self.name
+
+
+class Test(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    status = models.TextField(choices=AVAILABLE_STATUS, default="Active")
+
+    def __str__(self):
+        return self.name
+
+
+class Workload(models.Model):
+    test_date = models.DateField()
+    analyst = models.ForeignKey(Analyst, on_delete=models.CASCADE)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    comment = models.TextField(blank=True)
+    status = models.TextField(choices=WORKLOAD_STATUS, default="To Do")
+
+    class Meta:
+        ordering = ['test_date']
+
+    def __str__(self):
+        return f"{self.test_date},{self.analyst},{self.test}"
--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from datetime import datetime, timedelta
 
-# Create your models here.
+
+BATCH_STATUS = (("To Test", "To Test"), ("Approved", "Approved"))
+MATERIAL_STATUS = (("Active", "Active"), ("Inactive", "Inactive"))
+
+
+class Material(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    status = models.TextField(choices=AVAILABLE_STATUS, default="Active")
+
+    def __str__(self):
+        return self.name
+
+
+class Batch(models.Model):
+    priority = models.BooleanField(default=False)
+    batch = models.CharField(max_length=10, unique=True)
+    material = models.ForeignKey(Material, on_delete=models.PROTECT)
+    booked_in = models.DateField(auto_now_add=True)
+    comments = models.TextField(blank=True)
+    status = models.TextField(choices=BATCH_STATUS, default="To Test")
+
+    class Meta:
+        ordering = ['booked_in']
+
+    def __str__(self):
+        return self.batch
+
+    def due_date(self):
+        return self.booked_in + timedelta(7)
